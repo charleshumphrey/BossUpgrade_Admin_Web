cc66bc4eb8f16d3e47d96b4de7df5daf65f54586
Charles Humphrey <charleshumphreygarcia05@gmail.com>
2025-05-15 21:30:34 +0800

    Modified start.sh

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@ set -e
 echo "🔥 Decoding Firebase credentials from environment"
 
 # CLEAR ALL CACHES BEFORE CREATING THEM
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
-php artisan view:clear
+# php artisan config:clear
+# php artisan cache:clear
+# php artisan route:clear
+# php artisan view:clear
 
 echo "⚙️ Caching Laravel configs..."
 php artisan config:cache
